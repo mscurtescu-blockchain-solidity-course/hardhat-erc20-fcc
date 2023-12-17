e184d3e7652b05a017c097883960b368edde2076
Marius Scurtescu <marius.scurtescu@gmail.com>
2023-12-17 15:33:16 -0800

    implement tokens

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,41 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+require('hardhat-deploy');
+require("dotenv").config();
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+
 module.exports = {
-  solidity: "0.8.19",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      // // If you want to do some forking, uncomment this
+      // forking: {
+      //   url: MAINNET_RPC_URL
+      // },
+      chainId: 31337,
+    },
+    localhost: {
+      chainId: 31337,
+    },
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      //accounts: {
+      //     mnemonic: MNEMONIC,
+      // },
+      saveDeployments: true,
+      chainId: 11155111,
+      blockConfirmation: 6,
+    },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+    },
+    user1: {
+      default: 1,
+    },
+  },
+  solidity: "0.8.20",
 };
